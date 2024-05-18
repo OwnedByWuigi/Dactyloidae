b166ec94ae33d9315e798586c469eb9d4685dbd2
win7-7 <win7-7@users.noreply.github.com>
2024-05-18 23:05:05 +0300

    Revert "Trying add overrides back."
    This reverts commit 2b77ab0100757e24a302db23e4ba692671d83ccb.

--- xpcom/glue/nsThreadUtils.cpp
+++ xpcom/glue/nsThreadUtils.cpp
@@ -286,7 +286,7 @@ public:
   {
   }
 
-  NS_IMETHOD Run() override
+  NS_IMETHOD Run()
   {
     if (!mRunnable) {
       return NS_OK;
@@ -304,7 +304,7 @@ public:
     runnable->Run();
   }
 
-  void SetTimer(uint32_t aDelay, nsIEventTarget* aTarget) override
+  void SetTimer(uint32_t aDelay, nsIEventTarget* aTarget)
   {
     MOZ_ASSERT(aTarget);
     MOZ_ASSERT(!mTimer);
