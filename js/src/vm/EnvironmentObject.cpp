6ec6324056f8a975fe216e4973c58add48adf70b
win7-7 <win7-7@users.noreply.github.com>
2025-12-30 01:37:40 +0200

    1331452 -  jit-test/tests/debug/wasm-06.js failures with CGC builds
    1331452 - Keep scope in a slot in WasmFunctionCallObjects.
    
    1331452 - Followup: hazard fix.

--- js/src/vm/EnvironmentObject.cpp
+++ js/src/vm/EnvironmentObject.cpp
@@ -633,7 +633,7 @@ const Class WasmFunctionCallObject::class_ = {
 };
 
 /* static */ WasmFunctionCallObject*
-WasmFunctionCallObject::createHollowForDebug(JSContext* cx, WasmFunctionScope* scope)
+WasmFunctionCallObject::createHollowForDebug(JSContext* cx, Handle<WasmFunctionScope*> scope)
 {
     RootedObjectGroup group(cx, ObjectGroup::defaultNewGroup(cx, &class_, TaggedProto(nullptr)));
     if (!group)
@@ -652,6 +652,7 @@ WasmFunctionCallObject::createHollowForDebug(JSContext* cx, WasmFunctionScope* s
 
     Rooted<WasmFunctionCallObject*> callobj(cx, &obj->as<WasmFunctionCallObject>());
     callobj->initEnclosingEnvironment(&cx->global()->lexicalEnvironment());
+    callobj->initReservedSlot(SCOPE_SLOT, PrivateGCThingValue(scope));
 
     return callobj;
 }
