e75935a793d35bc0e4773d49bee4ebeb76b06929
win7-7 <win7-7@users.noreply.github.com>
2025-12-29 15:26:32 +0200

    Bug 1332772 - Baldr: tweak memory/table limits
    1332772 - Baldr: tweak memory/table limits

--- js/src/wasm/AsmJS.cpp
+++ js/src/wasm/AsmJS.cpp
@@ -1854,7 +1854,7 @@ class MOZ_STACK_CLASS ModuleValidator
     bool declareFuncPtrTable(Sig&& sig, PropertyName* name, uint32_t firstUse, uint32_t mask,
                              uint32_t* index)
     {
-        if (mask > MaxTableLength)
+        if (mask > MaxTableInitialLength)
             return failCurrentOffset("function pointer table too big");
         uint32_t sigIndex;
         if (!newSig(Move(sig), &sigIndex))
