53eb4e1f310d0e7884c35d83bc77fdf5720c7ff7
win7-7 <win7-7@users.noreply.github.com>
2026-01-04 21:04:13 +0200

    Sigh..
    Sigh..

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -4781,7 +4781,7 @@ NextIncomingCrossCompartmentPointer(JSObject* prev, bool unlink)
 }
 
 void
-js::gc::DelayCrossCompartmentGrayMarking(JSObject* src)
+js::DelayCrossCompartmentGrayMarking(JSObject* src)
 {
     MOZ_ASSERT(IsGrayListObject(src));
 
